471fcbc707d3ae410b88cdc6873d83928061b793
BarbaraGuimaraes21 <bahcamargo21@gmail.com>
2025-08-31 15:02:57 -0300

    feat: Adicionada lógica de processamento de dados e geração de relatório

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def obter_dados_usuarios():
     try:
@@ -8,6 +9,7 @@ def obter_dados_usuarios():
     except requests.exceptions.RequestException as e:
         print(f"Erro ao buscar usuários: {e}")
         return None
+    pass
 
 def obter_dados_todos():
     try:
@@ -17,6 +19,33 @@ def obter_dados_todos():
     except requests.exceptions.RequestException as e:
         print(f"Erro ao buscar tarefas: {e}")
         return None
+    pass
+
+def processar_dados(users,todos):
+    if not users or not todos:
+        return None
+
+    user_tasks = {}
+    for user in users:
+        user_tasks[user['id']] = {'completed': 0, 'pending': 0}
+
+    for todo in todos:
+        user_id = todo['userId']
+        if user_id in user_tasks:
+            if todo['completed']:
+                user_tasks[user_id]['completed'] += 1
+            else:
+                user_tasks[user_id]['pending'] += 1
+
+    report = []
+    for user in users:
+        tasks = user_tasks.get(user['id'],{'completed': 0, 'pending': 0})
+        report.append({
+            "user_id": user['id'],
+            "title": f"{user['name']} tasks",
+            "body": f"completed_tasks: {tasks['completed']}, pending_tasks:{tasks['pending']}"
+        })
+    return report
 
 if __name__ == "__main__":
     users = obter_dados_usuarios()
@@ -24,5 +53,8 @@ if __name__ == "__main__":
 
     if users and todos:
         print("Dados de usuários e tarefas obtidos com sucesso!")
+        final_report = processar_dados(users, todos)
+        if final_report:
+            print("Relatório de tarefas criado com sucesso!")
     else:
         print("Falha ao obter dados.")
